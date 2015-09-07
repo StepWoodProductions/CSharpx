6855e6b3411fc8f31b14aecb9ecca9bf082b19e7
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-09-07 16:03:20 +0200

    Used FsCheck <-> xunit integration

--- tests/CSharpx.Tests/MaybeTests.cs
+++ tests/CSharpx.Tests/MaybeTests.cs
@@ -12,24 +12,20 @@ namespace CSharpx.Tests
 {
     public class MaybeTests
     {
-        [Fact]
-        public void Constructing_a_monadic_integer_value_allows_the_same_to_be_extracted_unchanged()
+        [Property]
+        public void Constructing_a_monadic_integer_value_allows_the_same_to_be_extracted_unchanged(int x)
         {
-            Prop.ForAll<int>(
-                x =>
-                {
-                    var maybeInt = Maybe.Return(x);
-                    switch (maybeInt.Tag)
-                    {
-                        case MaybeType.Just:
-                            ((Just<int>)maybeInt).Value.ShouldBeEquivalentTo(x);
-                            break;
-                        default:
-                            default(int).ShouldBeEquivalentTo(x);
-                            maybeInt.Should().BeOfType<Nothing<int>>();
-                            break;
-                    }
-                }).QuickCheckThrowOnFailure();
+            var maybeInt = Maybe.Return(x);
+            switch (maybeInt.Tag)
+            {
+                case MaybeType.Just:
+                    ((Just<int>)maybeInt).Value.ShouldBeEquivalentTo(x);
+                    break;
+                default:
+                    default(int).ShouldBeEquivalentTo(x);
+                    maybeInt.Should().BeOfType<Nothing<int>>();
+                    break;
+            }
         }
     }
 }
